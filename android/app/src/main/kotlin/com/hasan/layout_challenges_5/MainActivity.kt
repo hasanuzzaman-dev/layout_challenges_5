a6e55d0ba02196027aa0c3d26f17f6fe542be7e1
Hasan <hasanuzzaman.dev@gmail.com>
2021-06-08 15:41:01 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/hasan/layout_challenges_5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hasan.layout_challenges_5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: double.infinity,
+                width: 100,
+                color: Colors.green,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.blue,
+                  )
+                ],
+              ),
+              Container(
+                height: double.infinity,
+                width: 100,
+                color: Colors.yellow[900],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
